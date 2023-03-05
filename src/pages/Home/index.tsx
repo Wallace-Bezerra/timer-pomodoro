abaf87bb680bd8028e9272f0d979bb1ed6cd158c
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2023-03-04 21:19:17 -0300

    feat: countdown decrement

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import { Play } from 'phosphor-react'
 import minus from '../../assets/Minus.svg'
 import plus from '../../assets/Plus.svg'
-import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { differenceInSeconds } from 'date-fns'
 import * as zod from 'zod'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   CountDownContainer,
   FormContainer,
@@ -26,6 +27,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 export const Home = () => {
   const {
@@ -46,20 +48,41 @@ export const Home = () => {
 
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [ActiveCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondPassed, setAmountSecondPassed] = useState(0)
   const ActiveCycle = cycles.find((cycle) => {
     return cycle.id === ActiveCycleId
   })
+  useEffect(() => {
+    if (ActiveCycle) {
+      setInterval(() => {
+        setAmountSecondPassed(
+          differenceInSeconds(new Date(), ActiveCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [ActiveCycle])
 
+  const totalSeconds = ActiveCycle ? ActiveCycle.minutesAmount * 60 : 0
+  const currentSeconds = ActiveCycle ? totalSeconds - amountSecondPassed : 0
+  const amountMinutes = ActiveCycle ? Math.floor(currentSeconds / 60) : 0
+  const amountSeconds = ActiveCycle ? currentSeconds % 60 : 0
+
+  const minutes = String(amountMinutes).padStart(2, '0')
+  const seconds = String(amountSeconds).padStart(2, '0')
+
+  console.log(minutes, 'total de minutos')
+  console.log(seconds, 'total de segundos')
   const handleFormSubmit = (data: newCicleFormData) => {
-    console.log(data)
     const id = new Date().getTime().toString()
     const newCycle: Cycle = {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+
     reset()
   }
   const watchForm = watch('task')
@@ -126,11 +149,11 @@ export const Home = () => {
         </FormContainer>
 
         <CountDownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator id="separator">:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountDownContainer>
 
         <button type="submit" disabled={isSubmitDisabled}>
